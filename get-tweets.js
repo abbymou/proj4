2cd37ba53dd95e89aa03065ceff9815c1d301c4f
abbymou <abbymou@live.unc.edu>
2019-11-18 13:38:54 -0500

    connecting

--- get-tweets.js
+++ get-tweets.js
@@ -10,7 +10,17 @@ const gc = new Storage({
   projectId: '487gmAM-b6274e4d192d'
 });
 
-gc.getBuckets().then(x => console.log(x));
+//gc.getBuckets().then(x => console.log(x));
+
+// saving file to GCS
+const stream     = require('stream'),
+      dataStream = new stream.PassThrough(),
+      gcFile     = gc.bucket('api-project-am').file('tweets.json')
+
+// name of GCS bucket
+const storageBucket = gc.bucket('api-project-am');
+//console.log(storageBucket);
+
 
 // Set up your search parameters
 const params = {
@@ -37,23 +47,34 @@ T.get('search/tweets', params, (err, data, response) => {
     tweets.push({ id: data.statuses[i].id_str, userName : data.statuses[i].user.name, screenName : data.statuses[i].user.screen_name, profileImage: data.statuses[i].user.profile_image_url_https, text: data.statuses[i].text });
     // var id = { id: data.statuses[i].id_str }
   };
+
   console.log(tweets);
   var completeData = JSON.stringify(tweets);
-  fs.writeFileSync('tweets.json', completeData);
+  var thePath = __dirname;
+  console.log(__dirname);
+  console.log(thePath);
+  var theFile = __dirname + '/tweetsVar.json'
+  //fs.writeFileSync('tweets.json', completeData);
   console.log(completeData);
   console.log('----- saved as tweets.json -----');
 
+  //saving file to GCS
+  dataStream.push(completeData)
+  dataStream.push(null)
 
-  /*tweetsId.map(tweetId => {
-    T.post('favorites/create', tweetId, (err, response) => {
-      if(err){
-        return console.log(err[0].message);
-      }
+  function saveFile(){
+    console.log('saving file...');
+  return new Promise((resolve, reject) => {
+    dataStream.pipe(gcFile.createWriteStream({
+      resumable  : false,
+      validation : false,
+      metadata   : {'Cache-Control': 'public, max-age=31536000'}
+    }))
+  })
+  }
 
-      const username = response.user.screen_name;
-      const favoritedTweetId = response.id_str;
-      console.log(`Favorited: https://twitter.com/${username}/status/${favoritedTweetId}`);
+  saveFile();
+  console.log("saved to GCS");
+  console.log("https://storage.cloud.google.com/api-project-am/tweets.json");
 
-    });
-  });*/
 })
